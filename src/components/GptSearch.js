1d6d40c08e1fc1bfffe0eeb44b4eaf1756a43bd1
JagneetJSG <jagneetsinghgill2@gmail.com>
2025-06-10 00:04:06 +0530

    prod app refresh fix, css fix-ios devices, images deleted

--- src/components/GptSearch.js
+++ src/components/GptSearch.js
@@ -5,11 +5,11 @@ import TmdbSuggestions from "./TmdbSuggestions";
 const GptSearch = () => {
   const gptNames = useSelector(store=> store?.gptSearch?.gptNames);
   return (
-    <div className='relative pt-40 mb-10 z-10 justify-items-center'>
-      <div className='w-11/12 md:w-8/12 mb-6 p-4 md:p-8 bg-black'>
+    <div className='relative pt-40 mb-10 z-10'>
+      <div className='w-11/12 md:w-8/12 mb-6 p-4 md:p-8 bg-black ml-auto mr-auto'>
         <GptSearchBar />
       </div>
-      {gptNames && <div className='w-11/12 md:w-8/12 pb-6 bg-black bg-opacity-80'>
+      {gptNames && <div className='w-11/12 md:w-8/12 pb-6 bg-black bg-opacity-80 ml-auto mr-auto'>
         <TmdbSuggestions />
       </div>}
     </div>
